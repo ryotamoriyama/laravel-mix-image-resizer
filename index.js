c8b62536ae271e8f5080c624430b356f65d6d96f
RyotaMoriyama <info@ryotamoriyama.com>
2020-02-16 20:56:20 +0900

    First Commit

---
+++ index.js
@@ -0,0 +1,89 @@
+const mix = require('laravel-mix')
+const fs = require('fs-extra')
+const path = require('path')
+const glob = require('glob')
+const sharp = require('sharp')
+const imagemin = require('imagemin')
+const imageminJpegtran = require('imagemin-jpegtran')
+const imageminPngquant = require('imagemin-pngquant')
+const imageminWebp = require('imagemin-webp')
+
+const defaultOptions = {
+    disable: false,
+    from: 'resources',
+    to: 'public',
+    sizes: [
+        414,
+        728,
+        1024,
+        1280,
+        1456,
+        1600
+    ],
+    webp: true,
+    imageminPngquantOptions: {
+        quality: [0.3, 0.5]
+    },
+    imageminWebpOptions: {
+        quality: 50
+    },
+}
+
+class ImageResizer {
+    register(extraOptions = {}) {
+        const {
+            disable,
+            sizes,
+            from,
+            to,
+            webp,
+            imageminPngquantOptions,
+            imageminWebpOptions,
+        } = Object.assign(defaultOptions, extraOptions)
+
+        if (disable) return
+
+        sizes.sort((a, b) {
+            if (a > b) {
+                return 1
+            } else {
+                return -1
+            }
+        })
+        fs.copySync(from, to)
+        const images = glob.sync(to + '/**/*').forEach((imagePath) => {
+            if (imagePath.match(/\.(jpe?g|png|gif)$/i) === null) {
+                return
+            }
+
+            let {root, dir, base, ext, name} = path.parse(imagePath)
+            sizes.forEach((w) => {
+                if (width < w) {
+                    return
+                }
+                sharp(imagePath)
+                    .resize(w)
+                    .toFile(dir + '/' + name + '-resized-' + w + ext)
+            })
+
+            imagemin([imagePath, dir + '/' + name + '-resized-*'], {
+                destination: dir,
+                plugins: [
+                    imageminJpegtran(),
+                    imageminPngquant(imageminPngquantOptions),
+                ],
+            })
+
+            if (webp) {
+                imagemin([imagePath, dir + '/' + name + '-resized-*'], {
+                    destination: dir,
+                    plugins: [
+                        imageminWebp(imageminWebpOptions)
+                    ],
+                })
+            }
+        })
+    }
+}
+
+mix.extend('ImageResizer', new ImageResizer())
